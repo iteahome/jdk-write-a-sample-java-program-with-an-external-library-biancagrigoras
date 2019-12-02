b1850d841d4b8f4a759b3f3be7acabe944d808f9
biancagrigoras <49443019+biancagrigoras@users.noreply.github.com>
2019-12-02 19:29:11 +0200

    @biancagrigoras
    joda

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package com.example.helloworld;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        DateTime date = new DateTime();
+        System.out.println("date = " + date);
+
+    }
+
+}
